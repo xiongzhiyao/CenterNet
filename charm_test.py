5d6732326439439bbae3a648c1b5b34be097edb0
xiongzhiyao <xzy1994@hotmail.com>
2019-05-29 22:42:27 +0000

    first test of running vscode remotely

---
+++ charm_test.py
@@ -0,0 +1,8 @@
+import torch
+import numpy as np
+
+print(torch.cuda.get_device_name(0))
+print("Hello World，with plotting world")
+
+
+print("now try something for the source tree")
---
+++ remote_bash.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python charm_test.py
