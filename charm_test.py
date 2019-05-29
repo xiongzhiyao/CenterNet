89f3436c060f54a542830bbfb425ea3ce72fa889
xiongzhiyao <xzy1994@hotmail.com>
2019-05-29 23:11:43 +0000

    pure test

--- charm_test.py
+++ charm_test.py
@@ -4,3 +4,4 @@ import numpy as np
 print(torch.cuda.get_device_name(0))
 print("Hello World，with plotting world")
 print("now lets try the git and source tree")
+print("I just want change another line")
