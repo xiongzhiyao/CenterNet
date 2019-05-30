3c949d982285240788fa631ca0d694c27e680133
xiongzhiyao <xzy1994@hotmail.com>
2019-05-30 00:19:52 +0000

    tested .env, not sure whther it has solved pylint issue yet

--- charm_test.py
+++ charm_test.py
@@ -1,6 +1,10 @@
 import torch
 import numpy as np
+import os
+from opts import opts
 
+print(opts)
+print(os.environ['PYTHONPATH'])
 print(torch.cuda.get_device_name(0))
 print("Hello World，with plotting world")
 print("now lets try the git and source tree")
